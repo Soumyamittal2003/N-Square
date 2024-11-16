3e79f4944d21b75e62deda7ccf9c051f9bb1c184
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-11-16 22:54:26 +0530

    deployment

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../utils/axiosInstance";
+import axiosInstance from "../utils/axiosinstance.jsx";
 
 const AuthContext = createContext();
 
