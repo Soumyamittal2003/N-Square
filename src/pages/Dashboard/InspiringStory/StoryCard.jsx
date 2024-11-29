59cebe05e72b83a5710c02c3da7bd41bc95182b2
Aadarsh soni <imaadarshsoni@gmail.com>
2024-11-29 16:35:00 +0530

    test2

--- src/pages/Dashboard/InspiringStory/StoryCard.jsx
+++ src/pages/Dashboard/InspiringStory/StoryCard.jsx
@@ -1,32 +1,28 @@
-// import React from "react";
-import { Link } from "react-router-dom";
+import arrowBlockUp from "../../../assets/icons/arrow-block-up.svg";
+import arrowBlockdown from "../../../assets/icons/arrow-block-down.svg";
 
-const StoryCard = ({ story, currentUserId }) => {
+const StoryCard = ({ title, description }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4">
-      {/* Story Title */}
-      <h3 className="text-lg font-semibold text-gray-800">{story.title}</h3>
-
-      {/* Story Author */}
-      <p className="text-sm text-gray-600">
-        By{" "}
-        {story.createdBy
-          ? `${story.createdBy.firstName} ${story.createdBy.lastName}`
-          : "Unknown Author"}
-      </p>
-
-      {/* Story Content Preview */}
-      <p className="text-sm text-gray-700 mt-2">
-        {story.content?.slice(0, 100)}...
-      </p>
-
-      {/* Link to full story */}
-      <Link
-        to={`/stories/${story._id}`}
-        className="text-blue-500 hover:text-blue-700 mt-2 inline-block"
-      >
-        Read more
-      </Link>
+    <div className="bg-white border  shadow-lg rounded-lg p-4 flex flex-col items-center text-center">
+      <div className="bg-gray-300 rounded-full w-16 h-16 flex items-center justify-center">
+        {/* Placeholder for a logo or avatar */}
+        <span className="text-white font-bold text-lg">{title.charAt(0)}</span>
+      </div>
+      <h3 className="mt-2 text-lg font-semibold">{title}</h3>
+      <p className="mt-2 text-sm text-gray-600">{description}</p>
+      <div className="flex justify-between mt-4 p-2 text-sm ">
+        <div className="flex items-center gap-3">
+          <button className="flex gap-1 font-semibold justify-center">
+            <img src={arrowBlockUp}></img>
+            <span>63K</span>
+          </button>
+          <button className="flex gap-1 font-semibold justify-center">
+            <img src={arrowBlockdown}></img>
+            <span>13K</span>
+          </button>
+        <button className="px-4 py-1 ml-2 bg-blue-600 text-white  rounded-full text-xs font-semibold">Follow</button>
+      </div>
+    </div>
     </div>
   );
 };
