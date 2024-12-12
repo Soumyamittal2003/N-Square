08587b36fc1c3251b3f0712b91475839fcc422cc
Smile <singhswapnil060303@gmail.com>
2024-12-12 09:08:19 +0530

    Add RightSidebar component to Funds page and adjust layout

--- src/pages/Admin/Funds/Funds.jsx
+++ src/pages/Admin/Funds/Funds.jsx
@@ -1,8 +1,10 @@
 import DonationContent from "./DonationContent";
+import RightSidebar from "./RightSidebar";
 
 const Funds = () => (
   <div className="flex justify-between bg-white-100 h-[10%] p-6">
     <DonationContent />
+    <RightSidebar />
   </div>
 );
 
